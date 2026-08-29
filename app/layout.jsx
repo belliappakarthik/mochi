98ea4f70fcb45efb21f67e70e5c0b534e11e6857
belliappakarthik <belliappa.thams@gmail.com>
2026-08-29 10:20:26 +0530

    Add app layout

---
+++ app/layout.jsx
@@ -0,0 +1,3 @@
+import './globals.css';
+export const metadata={title:'Mochi 🐶',description:'A tiny dog with a huge personality'};
+export default function Layout({children}){return <html lang="en"><body>{children}</body></html>}
